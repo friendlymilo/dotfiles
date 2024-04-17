e52d3f49ddad2973cc90253fe7e9387d61ce1c32
friendlymilo <dontthreadonme.x@proton.me>
2024-04-17 18:48:53 -0300

    nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,16 @@
 return {
   -- add gruvbox
   -- { "ellisonleao/gruvbox.nvim" },
-
+  {
+    "tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
